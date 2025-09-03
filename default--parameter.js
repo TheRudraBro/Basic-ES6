8a8e5be47bf817985a0fc2e35b943bbb5a6f9141
TheRudraBro <rudramraj22@gmail.com>
2025-09-03 13:10:42 +0600

    first commit

---
+++ default--parameter.js
@@ -0,0 +1,38 @@
+function sum(num1, num2=0)// num 1, num 2 - parameter
+{
+    const total = num1 + num2;
+    // console.log(num1, num2,total);
+}
+// NAN - Not a number
+// sum(10,29); // 10, 29 is arguments
+
+
+function multiply(num1, num2=1)
+{
+    const result = num1 * num2;
+    console.log(num1, num2, result);
+}
+multiply(19);
+
+/**
+ * add / sub - 0
+ * 
+ * multiply - 1
+ *
+ * string - ""
+ * 
+ * boolean - false
+ *
+ * object - {}
+ *
+ * array - []
+ *
+ * function - undefined
+ * \ - NaN
+ *
+ * null - null
+ *
+ * symbol - undefined
+ * 
+ * bigint - 0n
+ */
---
+++ let-const.js
@@ -0,0 +1,18 @@
+// es6
+var device = 'Laptop';
+let name = "John";
+const countryName = 'John' + name;
+console.log(countryName);
+
+let price = 120;
+price= 150;
+console.log(price);
+
+
+// 
+const charge = 50;
+
+
+const dress = ['shirt', 'pants', 'jacket'];
+dress.push('suit');
+console.log(dress)
